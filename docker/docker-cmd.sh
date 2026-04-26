4174778e810c4735fd78e080dad207a7e801ab87
James Knight <git@jdknight.me>
2026-04-26 10:27:32 -0400

    use more appropriate source call for posix shell
    Signed-off-by: James Knight <git@jdknight.me>

--- docker/docker-cmd.sh
+++ docker/docker-cmd.sh
@@ -2,7 +2,8 @@
 set -e
 
 # on start, always run nfsn-ddns
-source /opt/venv/bin/activate
+# shellcheck source=/dev/null
+. /opt/venv/bin/activate
 nfsn-ddns --cache $NFSN_DDNS_EXTRA_ARGS
 
 # start cron to schedule invokes of nfsn-ddns
